2b1c436afe9b5ef71fb307337b7b471ad43a277d
elangoram1998 <30955305+elangoram1998@users.noreply.github.com>
2018-01-20 21:36:38 +0530

    Create Display_Even_Numbers.java

---
+++ Beginner/set 2/Display_Even_Numbers.java
@@ -0,0 +1,24 @@
+
+import java.util.Scanner;
+
+public class Find_Even {
+
+	public static void main(String[] args) {
+		int a,b,i;
+		Scanner sc=new Scanner(System.in);
+		System.out.println("enter the first intervel:");
+		a=sc.nextInt();
+		System.out.println("enter the second intervel:");
+		b=sc.nextInt();
+		System.out.println("even numbrs:");
+		for(i=a+1;i<b;i++)
+		{
+			if(i%2==0)
+			{
+				System.out.println(i);
+			}
+		}
+
+	}
+
+}
