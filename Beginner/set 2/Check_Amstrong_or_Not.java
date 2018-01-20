1e097cc309c4d933d0a6b7a9f2473f49bd14b83b
elangoram1998 <30955305+elangoram1998@users.noreply.github.com>
2018-01-20 22:29:28 +0530

    Create Check_Amstrong_or_Not.java

---
+++ Beginner/set 2/Check_Amstrong_or_Not.java
@@ -0,0 +1,28 @@
+import java.util.Scanner;
+
+public class Amstrong {
+
+	public static void main(String[] args) {
+		int num,temp=0,digit;
+		Scanner sc=new Scanner(System.in);
+		System.out.println("enter the number:");
+		num=sc.nextInt();
+		int a=num;
+		while(num!=0)
+		{
+			digit=num%10;
+			temp=(digit*digit*digit)+temp;
+			num/=10;
+		}
+		if(temp==a)
+		{
+			System.out.println("amstrong number");
+		}
+		else
+		{
+			System.out.println("not a amstrong number");
+		}
+
+	}
+
+}
