f0273413339c42f69b8a0b5d964de20fb874faf3
elangoram1998 <30955305+elangoram1998@users.noreply.github.com>
2018-01-21 11:32:39 +0530

    Create Amstrong_number_in_Given_Interval.java

---
+++ Beginner/set 2/Amstrong_number_in_Given_Interval.java
@@ -0,0 +1,37 @@
+package sentense_case;
+
+import java.util.Scanner;
+
+public class ReverseNumber {
+	
+    public static void main(String[] args) 
+    {
+    	int num,digit,temp,i,a,b,count=0;
+    	Scanner sc=new Scanner(System.in);
+    	System.out.println("enter the first interval:");
+    	a=sc.nextInt();
+    	System.out.println("enter the second interval:");
+    	b=sc.nextInt();
+    	System.out.println("amstrong numbers are:");
+    	for(i=a+1;i<b;i++)
+    	{
+    	int c=i;
+    	temp=0;
+    	while(c!=0)
+    	{
+    		digit=c%10;
+    		temp=(digit*digit*digit)+temp;
+    		c/=10;
+    	}
+    	if(temp==i)
+    	{
+    		System.out.print(i+" ");
+    		count++;
+    	}
+    	}
+    	if(count==0)
+    	{
+    		System.out.println("no amstrong numbers between this intervals");
+    		}
+    }
+}
