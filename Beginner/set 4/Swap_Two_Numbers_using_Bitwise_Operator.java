7dc3903977838296f96d708433d014e4a3c01bc4
elangoram1998 <30955305+elangoram1998@users.noreply.github.com>
2018-01-23 20:29:07 +0530

    Create Swap_Two_Numbers_using_Bitwise_Operator.java

---
+++ Beginner/set 4/Swap_Two_Numbers_using_Bitwise_Operator.java
@@ -0,0 +1,19 @@
+import java.util.Scanner;
+
+public class Swap_Numbers {
+
+	public static void main(String[] args) {
+		int a ,b;
+		Scanner sc=new Scanner(System.in);
+		System.out.println("enter the first number:");
+		a=sc.nextInt();
+		System.out.println("enter the second number:");
+		b=sc.nextInt();
+		a=a^b;
+		b=a^b;
+		a=a^b;
+		System.out.println("swapped numbers:"+ a+" "+b);
+
+	}
+
+}
