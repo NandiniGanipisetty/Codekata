bf6ddd24cdc6c875e935aaefd2d3173e4fe5b33e
elangoram1998 <30955305+elangoram1998@users.noreply.github.com>
2018-01-23 19:03:54 +0530

    Create Count_Special_Characters.java

---
+++ Beginner/set 4/Count_Special_Characters.java
@@ -0,0 +1,30 @@
+import java.util.Scanner;
+
+public class Count_Special_char {
+	public static void fun(String special)
+	{
+		String str;
+		int i,j,count=0;
+		Scanner sc=new Scanner(System.in);
+		System.out.println("enter the string:");
+		str=sc.nextLine();
+		for(i=0;i<str.length();i++)
+		{
+			for(j=0;j<special.length();j++)
+			{
+				if(str.charAt(i)==special.charAt(j))
+				{
+					count++;
+				}
+			}
+		}
+		System.out.println("no of special characters:"+count);
+	}
+
+	public static void main(String[] args) {
+		fun("!\"#$%&'()*+,-./:;<=>?@[\\]^_`{|}~");
+		
+
+	}
+
+}
