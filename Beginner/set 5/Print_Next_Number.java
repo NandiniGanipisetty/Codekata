e4cfbd5032c511c9a44d4e399d026696dac0353b
elangoram1998 <30955305+elangoram1998@users.noreply.github.com>
2018-01-24 07:30:32 +0530

    Create Print_Next_Number.java

---
+++ Beginner/set 5/Print_Next_Number.java
@@ -0,0 +1,15 @@
+import java.util.Scanner;
+
+public class Print_Number {
+
+	public static void main(String[] args) {
+		int N;
+		Scanner sc=new Scanner(System.in);
+		System.out.println("enter the number:");
+		N=sc.nextInt();
+		N+=1;
+		System.out.println(N);
+
+	}
+
+}
