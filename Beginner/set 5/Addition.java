524ff538e5831642e37962383f163f3135350713
elangoram1998 <30955305+elangoram1998@users.noreply.github.com>
2018-01-24 07:50:55 +0530

    Create Addition.java

---
+++ Beginner/set 5/Addition.java
@@ -0,0 +1,16 @@
+import java.util.Scanner;
+
+public class Addition {
+
+	public static void main(String[] args) {
+		int a,b;
+		Scanner sc=new Scanner(System.in);
+		System.out.println("enter a:");
+		a=sc.nextInt();
+		System.out.println("enter b:");
+		b=sc.nextInt();
+		System.out.println("addition:"+(a+b));
+
+	}
+
+}
