67b570628cb05354c67bfe9281f3f9028c9341aa
elangoram1998 <30955305+elangoram1998@users.noreply.github.com>
2018-02-01 22:50:26 +0530

    Update Swap_Two_Numbers.java

--- Beginner/set 6/Swap_Two_Numbers.java
+++ Beginner/set 6/Swap_Two_Numbers.java
@@ -4,8 +4,9 @@ public class Swap {
 
 	public static void main(String[] args) {
 		Scanner sc = new Scanner(System.in);
-		System.out.println("enter the numbers:");
+		System.out.println("enter the first number:");
 		int a=sc.nextInt();
+		System.out.println("enter the second number:");
 		int b=sc.nextInt();
 		int temp;
 		temp=a;
