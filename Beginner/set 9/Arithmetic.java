96ede1e2418366c6f7cc19db07d9b4e804e8fe7b
elangoram1998 <30955305+elangoram1998@users.noreply.github.com>
2018-02-03 07:50:09 +0530

    Create Arithmetic.java

---
+++ Beginner/set 9/Arithmetic.java
@@ -0,0 +1,24 @@
+import java.util.Scanner;
+
+public class Arithmetic {
+
+	public static void main(String[] args) {
+		int a,b,c,d,e,f,g,h;
+		Scanner sc=new Scanner(System.in);
+		System.out.println("enter the numbers:");
+		a=sc.nextInt();
+		b=sc.nextInt();
+		c=sc.nextInt();
+		d=sc.nextInt();
+		e=sc.nextInt();
+		f=sc.nextInt();
+		g=sc.nextInt();
+		h=sc.nextInt();
+		System.out.println(a/b);
+		System.out.println(c%d);
+		System.out.println(e/f);
+		System.out.println(g%h);
+
+	}
+
+}
