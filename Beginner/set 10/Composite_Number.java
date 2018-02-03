44e12cce349c20963f046a8e8956a711df6973ce
elangoram1998 <30955305+elangoram1998@users.noreply.github.com>
2018-02-03 21:51:42 +0530

    Create Composite_Number.java

---
+++ Beginner/set 10/Composite_Number.java
@@ -0,0 +1,27 @@
+import java.util.Scanner;
+
+public class Composite {
+
+	public static void main(String[] args) {
+		int a,i,count=0;
+		Scanner sc=new Scanner(System.in);
+		a=sc.nextInt();
+		for(i=1;i<=a;i++)
+		{
+			if(a%i==0)
+			{
+				count++;
+			}
+		}
+		if(count>2)
+		{
+			System.out.println("yes");
+		}
+		else
+		{
+			System.out.println("no");
+		}
+
+	}
+
+}
