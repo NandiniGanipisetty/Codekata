4b593dced0ca06a7ce12f4294c66241031feb50b
elangoram1998 <30955305+elangoram1998@users.noreply.github.com>
2018-02-03 20:44:37 +0530

    Create Kabali.java

---
+++ Beginner/set 9/Kabali.java
@@ -0,0 +1,15 @@
+import java.util.Scanner;
+
+public class Ninja {
+
+	public static void main(String[] args) {
+		int a,b,i,c;
+		Scanner sc=new Scanner(System.in);
+		System.out.println("enter the numbers:");
+		a=sc.nextInt();
+		b=sc.nextInt();
+		c=b-a;
+		System.out.println(c);
+	}
+
+}
