c70914b1072b81d046343f0a05ab508e582a23ab
elangoram1998 <30955305+elangoram1998@users.noreply.github.com>
2018-02-04 21:36:40 +0530

    Create Kth_Smallest_Number.java

---
+++ Beginner/set 12/Kth_Smallest_Number.java
@@ -0,0 +1,33 @@
+import java.util.Scanner;
+
+public class Small {
+
+	public static void main(String[] args) {
+		int N,K,i,j,temp;
+		Scanner sc=new Scanner(System.in);
+		System.out.println("enter the size and k:");
+		N=sc.nextInt();
+		K=sc.nextInt();
+		int[] array=new int[N];
+		System.out.println("enter the numbers:");
+		for(i=0;i<N;i++)
+		{
+			array[i]=sc.nextInt();
+		}
+		for(i=0;i<N;i++)
+		{
+			for(j=i+1;j<N;j++)
+			{
+				if(array[i]>array[j])
+				{
+					temp=array[i];
+					array[i]=array[j];
+					array[j]=temp;
+				}
+			}
+		}
+		System.out.println(array[K-1]);
+
+	}
+
+}
