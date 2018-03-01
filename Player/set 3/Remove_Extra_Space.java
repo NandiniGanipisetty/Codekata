ddd5c310202f567c3a9bbf1486457eb8bf131144
elangoram1998 <30955305+elangoram1998@users.noreply.github.com>
2018-03-01 21:48:04 +0530

    Create Remove_Extra_Space.java

---
+++ Player/set 3/Remove_Extra_Space.java
@@ -0,0 +1,16 @@
+package remove_extra_space;
+
+import java.util.Scanner;
+
+public class Remove_Space {
+
+	public static void main(String[] args) {
+		String s;
+		Scanner sc=new Scanner(System.in);
+		System.out.println("enter the string:");
+		s=sc.nextLine();
+		System.out.println(s.replaceAll(" +", " "));
+
+	}
+
+}
