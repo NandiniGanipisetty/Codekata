a02d22a512ba0e89d07ee75a14ef5c183133e895
Elango R <30955305+elangoram1998@users.noreply.github.com>
2018-05-23 19:06:56 +0530

    Create Largest_SubString_isPalindrome_or_not.java

---
+++ Hunter/set 3/Largest_SubString_isPalindrome_or_not.java
@@ -0,0 +1,43 @@
+package max_length;
+
+import java.util.Scanner;
+
+public class SubString {
+
+	public static void main(String[] args) {
+		String N;
+		Scanner sc=new Scanner(System.in);
+		N=sc.nextLine();
+		System.out.println(longestPalindrome(N));
+	}
+	static String longestPalindrome(String str)
+	{
+		String s=str,S=null;
+		int max=0;
+		for(int i=0;i<=s.length();i++)
+		{
+			int len=s.substring(0,i).length();
+			if(isPalindrome(s.substring(0, i)))
+			{
+				if(max<len)
+				{
+					max=len;
+					S=s.substring(0,i);
+				}
+			}
+		}
+		return S;
+	}
+	static boolean isPalindrome(String str)
+	{
+		String rev=str;
+		StringBuilder sb=new StringBuilder(rev);
+		sb.reverse();
+		if(!(str.compareTo(sb.toString())==0))
+		{
+			return true;
+		}
+		return false;
+	}
+
+}
